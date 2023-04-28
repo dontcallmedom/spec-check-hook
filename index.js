afd7bdcc545b0ecb96a0ba8a7429a119a6887475
Dominique Hazael-Massieux <dom@w3.org>
2023-04-28 14:37:33 +0200

    Use PR URL as input to the overall process

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 const {listRemovedTargets} = require("./lib/list-removed-targets");
+const {parsePR} = require("./lib/parse-pr");
 
 if (require.main === module) {
-  const specSource = process.argv[2];
-  const specRepo = process.argv[3];
-  const updatedSpecUrl = process.argv[4];
-  const webrefPath = process.argv[5];
-  listRemovedTargets(specSource, specRepo, updatedSpecUrl, webrefPath).then(list => console.log(list)).catch(err => {
-    console.error(`Failed:`);
-    console.trace(err);
-    process.exit(2);
-  });
+
+  const GH_TOKEN = (() => {
+    try {
+      return require('./config.json').GH_TOKEN;
+    } catch (err) {
+      return process.env.GH_TOKEN;
+    }
+  })();
+
+  const prURL = process.argv[2];
+  const [,repoFullName, prNumber] = prURL.match(/https:\/\/github.com\/([^\/]+\/[^\/]+)\/pull\/([0-9]+)/);
+  
+  const webrefPath = process.argv[3];
+  parsePR(repoFullName, prNumber, GH_TOKEN)
+    .then(({spec, link: updatedSpecUrl}) => listRemovedTargets(spec, updatedSpecUrl, webrefPath))
+    .then(list => console.log(list)).catch(err => {
+      console.error(`Failed:`);
+      console.trace(err);
+      process.exit(2);
+    });
 }
