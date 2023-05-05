3e1f8e0166d20accf8b69dc6bc0d739009024588
Dominique Hazael-Massieux <dom@w3.org>
2023-05-05 08:19:43 +0200

    Ignore old pull requests based on base commit date
    close #4

--- test/gh-webhook.js
+++ test/gh-webhook.js
@@ -29,6 +29,7 @@ let server, req;
 const login = "ghtester";
 
 const prNumber = 42;
+const test_sha = "0123456789abcdef";
 const testPreviewLink = `https://pr-preview.s3.amazonaws.com/foo/repo/pull/${prNumber}.html`;
 const testWhatwgMultiPreviewLinks = [`https://whatpr.org/reponame-multi/${prNumber}/subpage.html`, `https://whatpr.org/reponame-multi/${prNumber}/subpage2.html`];
 const testWhatwgMultiPreviewIndex = `https://whatpr.org/reponame-multi/${prNumber}/index.html`;
@@ -63,7 +64,7 @@ function mockPreviewSpec(link) {
   }
 }
 
-const editPrPayload = (sender, repoFullname, date = new Date()) => {
+const editPrPayload = (sender, repoFullname) => {
   return {
     action: "edited",
     number: prNumber,
@@ -76,7 +77,9 @@ const editPrPayload = (sender, repoFullname, date = new Date()) => {
     },
     pull_request: {
       number: prNumber,
-      created_at: JSON.stringify(date)
+      base: {
+	sha: test_sha
+      }
     },
     sender: {
       login: sender
@@ -122,7 +125,7 @@ describe("the webhook server", function() {
   it("reacts to a PR edit from pr-preview bot on a single page spec", async () => {
     const spec = getSpec("single-page");
     const repo = spec.nightly.repository.split("/").slice(3).join("/");
-    ghMock.pr(repo, prNumber, testPreviewLink, spec.nightly.source_path);
+    ghMock.pr(repo, prNumber, testPreviewLink, spec.nightly.source_path, test_sha);
     ghMock.listComments(repo, prNumber, []);
     ghMock.prComment(repo, prNumber,
 		     formatReport(removedTargets(["https://example.com/single-page#valid1"]), spec));
@@ -139,7 +142,7 @@ describe("the webhook server", function() {
     const spec = getSpec("single-page");
     const repo = spec.nightly.repository.split("/").slice(3).join("/");
     const matchingComment = { user: { login }, body: ' data-sc-marker="removedtargets"' };
-    ghMock.pr(repo, prNumber, testPreviewLink, spec.nightly.source_path);
+    ghMock.pr(repo, prNumber, testPreviewLink, spec.nightly.source_path, test_sha);
     mockPreviewSpec(testPreviewLink);
     ghMock.listComments(repo, prNumber, [matchingComment]);
     const payload = editPrPayload("pr-preview[bot]", repo);
@@ -154,7 +157,7 @@ describe("the webhook server", function() {
   it("reacts to a PR edit from pr-preview bot on a multi page spec, but only on pages listed as changed", async () => {
     const spec = getSpec("multi-page");
     const repo = spec.nightly.repository.split("/").slice(3).join("/");
-    ghMock.pr(repo, prNumber, testWhatwgMultiPreviewLinks, spec.nightly.source_path);
+    ghMock.pr(repo, prNumber, testWhatwgMultiPreviewLinks, spec.nightly.source_path, test_sha);
     ghMock.listComments(repo, prNumber, []);
     ghMock.prComment(repo, prNumber,
 		     formatReport(removedTargets(["https://example.com/multi-page/subpage.html#valid1", "https://example.com/multi-page/subpage2.html#valid2"]), spec));
@@ -169,8 +172,8 @@ describe("the webhook server", function() {
   });
 
   it("ignores PR edits from pr-preview bot on pull requests older than the crawled version of the spec", async () => {
-    ghMock.pr("acme/repo", prNumber, testPreviewLink, "test.bs");
-    const payload = editPrPayload("pr-preview[bot]", "acme/repo", new Date("2000-01-01"));
+    ghMock.pr("acme/repo", prNumber, testPreviewLink, "test.bs", test_sha, new Date("2000-01-01"));
+    const payload = editPrPayload("pr-preview[bot]", "acme/repo");
     try {
       const res = await setupRequest(req, payload).expect(200);
     } catch (err) {
